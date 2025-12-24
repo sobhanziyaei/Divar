4bc4014445444d3f6ae42daf8bbd15d4317fa676
Sobhan <ziaey@gmail.com>
2025-12-24 08:02:09 +0330

    add swagger to project

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const swaggerConfig = require("./src/config/swagger.config");
 dotenv.config();
 
 main = async () => {
     const app = express();
     const port = process.env.PORT;
     require('./src/config/mongoose.config');
+
+    swaggerConfig(app);
 
     app.listen(port, () => {
         console.log("Server is running on http://localhost:3000");
--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,13 +1,24 @@
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
-const swaggerDocument = swaggerJsDoc({
-    swaggerDefinition: {
-        info: {
-            title: "divar-backend",
-            description: "divar-backend API Documentation",
-            version: "1.0.0",
-        }
-    },
-    apis: []
-})
+const swaggerConfig = (app) => {
+    const swaggerDocument = swaggerJsDoc({
+        swaggerDefinition: {
+            info: {
+                title: "divar-backend",
+                description: "divar-backend API Documentation",
+                version: "1.0.0",
+            }
+        },
+        apis: []
+    })
+
+    const swagger = swaggerUi.setup(swaggerDocument, {});
+    app.use("/", swaggerUi.serve, swagger);
+}
+
+
+
+//const swaggerConfig = swaggerUi.setup(swaggerDocument,{});
+
+module.exports = swaggerConfig;
