82047f39bbccae9f699009afef9e070327a1cfc8
Sobhan <ziaey@gmail.com>
2025-12-27 07:58:45 +0330

    add user model

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,15 @@
+const { Schema } = require("mongoose");
+
+const OTPSchema = new Schema({
+    code: { type: String, required: false, default: undefined },
+    expiresIn: { type: Number, required: false, default: 0 }
+})
+
+const UserSchema = new Schema({
+    fullName: { type: String, required: false },
+    mobile: { type: String, unique: true, required: true },
+    otp: { type: OTPSchema },
+    verifiedMobile: { type: Boolean, default: false, required: true }
+},
+    { timestamps: true }
+)
