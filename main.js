50ff4bcac42a1a1413a2b9c8fb279037299668e3
Sobhan <ziaey@gmail.com>
2025-12-30 08:46:20 +0330

    add exceptionHandler to project

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const swaggerConfig = require("./src/config/swagger.config");
+const notFoundHandle = require("./src/common/exception/not-found.handler");
+const allExceptionHandler = require("./src/common/exception/all-exception.handler");
 const mainRouter = require("./src/app.routes");
 dotenv.config();
 
@@ -12,8 +14,10 @@ main = async () => {
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
 
-    swaggerConfig(app);
     app.use(mainRouter);
+    swaggerConfig(app);
+    notFoundHandle(app);
+    allExceptionHandler(app);
 
     app.listen(port, () => {
         console.log("Server is running on http://localhost:3000");
---
+++ src/common/exception/all-exception.handler.js
@@ -0,0 +1,13 @@
+allExceptionHandler = (app) => {
+    app.use((err, req, res, next) => {
+        let status = err?.status ?? err?.statusCode ?? err?.code;
+        if (!status || isNaN(+status) || status > 511 || status < 200) {
+            status = 500;
+        }
+        res.status(status).json({
+            message: err?.message ?? err?.stack ?? "InternalServerError"
+        })
+    })
+}
+
+module.exports = allExceptionHandler;
