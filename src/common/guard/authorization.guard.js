5435f8db2b02861035d1dd8eb6ee70b0b12c995a
sobhan <sobhan.ziaee73@gmail.com>
2026-02-07 21:54:14 +0330

    feat: implement authorization guard and add authorization messages for error handling

---
+++ src/common/guard/authorization.guard.js
@@ -0,0 +1,27 @@
+const createHttpError = require("http-errors");
+const AuthorizationMessage = require("../messages/auth.message");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../../modules/user/user.model");
+require("dotenv").config();
+
+const Authorization = async (req, res, next) => {
+    try {
+        const token = req?.cookie?.access_token;
+        if (!token) throw new createHttpError.Unauthorized(AuthorizationMessage.Login);
+        const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        if (data?.id) {
+            const user = await UserModel.findById(data.id, { accessToken: 0, otp: 0 }).lean();
+            if (!user) throw new createHttpError.Unauthorized(AuthorizationMessage.NotFoundAccount);
+            req.user = user;
+            return next();
+        }
+
+        throw new createHttpError.Unauthorized(AuthorizationMessage.InValidToken);
+
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = Authorization;
---
+++ src/common/messages/auth.message.js
@@ -0,0 +1,9 @@
+const AuthorizationMessage = Object.freeze({
+    Login: "login on your account",
+    LoginAgain: "please login again",
+    Unauthorized: "unauthorized please login on your account",
+    NotFoundAccount: "not found account",
+    InValidToken: "token in invalid",
+});
+
+module.exports = AuthorizationMessage;
