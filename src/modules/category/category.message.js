362482e73a2640c89f1652921b7faef0814fba8c
sobhan <sobhan.ziaee73@gmail.com>
2026-02-22 20:41:31 +0330

    feat(category): enhance category service with parent category handling and error messaging

--- src/modules/category/category.message.js
+++ src/modules/category/category.message.js
@@ -1,5 +1,6 @@
 const CategoryMessage = Object.freeze({
     Created: "Category created successfully",
+    NotFound: "Category not found",
 });
 
 module.exports = CategoryMessage;
--- src/modules/category/category.service.js
+++ src/modules/category/category.service.js
@@ -1,5 +1,7 @@
 const autoBind = require("auto-bind");
 const CategoryModel = require("./category.model");
+const createHttpError = require("http-errors");
+const { isValidObjectId, Types } = require("mongoose");
 
 
 class CategoryService {
@@ -9,7 +11,25 @@ class CategoryService {
         this.#model = CategoryModel;
     }
 
-    async create(categoryDto){}
+    async create(categoryDto) {
+        if (categoryDto?.parent && isValidObjectId(categoryDto.parent)) {
+            const existCategory = await this.checkExistById(categoryDto.parent);
+            categoryDto.parent = existCategory._id;
+            categoryDto.parents = [
+                ... new Set(
+                    ([existCategory._id.toString()].concat(
+                        existCategory.parents.map(id => id.toString())
+                    )).map(id => new Types.ObjectId(id))
+                )
+            ]
+        }
+    }
+
+    async checkExistById(id) {
+        const category = await this.#model.findById(id);
+        if (!category) throw new createHttpError.NotFound(CategoryMessage.NotFound);
+        return category;
+    }
 }
 
 module.exports = new CategoryService();
